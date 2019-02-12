60ef464964fde692393727989094608bfe04d341
kyro46 <cjobst@wifa.uni-leipzig.de>
2019-02-12 14:52:23 +0100

    Language and Readme

---
+++ README.md
@@ -0,0 +1,28 @@
+# moodle_qformat_qti12
+Import questions with the IMS QTI 1.2 format into moodle. Tested with questionpools from ILIAS.
+
+## Supported questiontypes ##
+
+* Single Choice
+
+### Mapping for Single Choice ###
+
+
+| ILIAS | Moodle |
+| --- | --- |
+| Title | Question name |
+| Author | Created by |
+| Description | - |
+| Question | Question text |
+| Working Time | - |
+| Shuffle Answers | Shuffle the choices? |
+| [Highest point value] | Default mark | 
+| [qtype=singlechoice] | One or multiple answers [One answer only] |
+| -  | Number the choices? [No numbering] |
+| Answer Text | Choice |
+| Points | Grade[highest QTI-point value as base and then transformed to respective percentages] |
+| Correct solution | Combined feedback[For any correct response] |
+| At least one answer is not correct | Combined feedback[For any partially correct response] |
+| Feedback for every answer | Feedback [per choice] |
+| Hints TBD | Hints TBD
+| Taxonomies | Tags |
---
+++ lang/en/qformat_qti12.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qformat_qti12', language 'en'.
+ *
+ * @package    qformat_qti12
+ * @author     Christoph Jobst <cjobst@wifa.uni-leipzig.de>
+ * @copyright  2019, University Leipzig
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'IMS QTI 1.2 format (ILIAS)';
+$string['pluginname_help'] = 'Imports supported questions from the QTI 1.2 (ILIAS) format and converts them into the Moodle equivalent.';
+$string['unknownquestiontype'] = 'Question type {$a} is not supported by this version of the QTI 1.2 (ILIAS) import';
